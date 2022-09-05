dc2e5bf5a29cf653be9deb812627e2bf1e338451
Tony Reurink <lochhollows@pop-os.localdomain>
2022-09-05 09:27:27 -0400

    idk

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import {state} from '@lib/store';
-    import {goto} from '$app/navigation';
-    import { onMount, beforeUpdate } from 'svelte';
+    import { state } from "@lib/store"
+    import { goto } from "$app/navigation"
+    import { onMount, beforeUpdate } from "svelte"
 
     let email: string,
-        password: string = '';
+        password: string = ""
 
     beforeUpdate(() => {
-        if ($state.account) goto('/');
+        if ($state.account) goto("/")
     })
 
     const login = async () => {
@@ -16,17 +16,17 @@
         }
         try {
             await state.login(email, password)
-            goto('/')
+            goto("/")
         } catch (error) {
             console.log(error)
         }
     }
 
-    $: checkform = email !== "" && password !== "";
+    $: checkform = email !== "" && password !== ""
 </script>
 
 <form on:submit|preventDefault={login}>
     <input type="email" id="email" bind:value={email} />
     <input type="password" id="password" bind:value={password} />
     <button type="submit" disabled={!checkform}>Login</button>
- </form>
+</form>
